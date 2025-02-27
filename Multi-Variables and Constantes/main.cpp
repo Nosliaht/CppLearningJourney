29d40d9663e291d1125c2d1effc0b159ebddf0a2
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-02-27 05:42:45 -0300

    Multi-Variables and Constante Class on Cpp

---
+++ Multi-Variables and Constantes/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+#define birthday cout << "Dev Mectick Birthday: ";
+int main()
+{
+    birthday int day = 25, month = 01, year = 2002;
+    cout << day << "/"
+         << month << "/"
+         << year << endl;
+    system("pause");
+    return 0;
+}
