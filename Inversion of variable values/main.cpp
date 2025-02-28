658847e7c76901817c0e6d44344c4bf25d337459
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-02-28 06:54:24 -0300

    Inversion of variable values class

---
+++ Inversion of variable values/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num = 10;
+    int num2 = -10;
+
+    // cout << "Before Inversion: " << num << "\n";
+    // num = num * -1;
+    // cout << "After Inverio: " << num << "\n\n";
+
+    // cout << "Before Inversion: " << num << "\n";
+    // num *= -1;
+    // cout << "After Inverio: " << num << "\n\n";
+
+    // cout << -num; Only Inversion on Cout;
+
+    // cout << "Before Inversion: " << num2 << "\n";
+    // num2 = num2 * -1;
+    // cout << "Before Inversion: " << num2 << "\n";
+
+    // cout << "Before Inversion: " << num2 << "\n";
+    // num2 *= -1;
+    // cout << "Before Inversion: " << num2 << "\n";
+    // return 0;
+
+    cout << -num2; // Only Inversion on Cout;
+}
