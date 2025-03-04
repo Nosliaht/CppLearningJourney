190e33e7708f4ec37100edbc5cf0478dd6f5c33d
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-03-04 13:57:55 -0300

    While Loop

---
+++ While/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int number = 0;
+    while (number <= 0)
+    {
+        cout << "Enter a number greater than zero: ";
+        cin >> number;
+    }
+
+    cout << number << " is greater than zero.";
+
+    return 0;
+}
