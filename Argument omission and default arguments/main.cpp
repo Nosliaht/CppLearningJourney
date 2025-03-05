edd6c08152feaa85f6252771ff6ebb25048269ae
Thailson Júlio <113474315+Thailson13@users.noreply.github.com>
2025-03-05 17:11:36 -0300

    If the caller omits the argument in the function call, the default value is used from the fucion prototype

---
+++ Argument omission and default arguments/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+void print(string txt = "[DEV]Mestick"); // If the caller omits the argument in the function call, this value will be the default.
+int main()
+{
+    print();
+    print("Is");
+    print("The");
+    print("Best");
+    return 0;
+}
+
+void print(string txt)
+{
+    cout << txt << "\n";
+}
